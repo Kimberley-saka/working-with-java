cea5ab63b64a50d4eb4de93270b7b6ff4cbb99fb
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-16 13:10:18 +0200

    conditionals

---
+++ Basics.java
@@ -0,0 +1,9 @@
+public class Basics {
+    public static void main (String[] args) {
+       int votingAge = 18;
+       //tenary if..else
+       String canVote = (votingAge >= 18) ? "Can Vote" : "Cannot Vote";
+
+       System.out.println(canVote);
+    }
+}
